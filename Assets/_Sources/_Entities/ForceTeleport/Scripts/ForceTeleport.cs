5288e0da6a46d1ca5989d2cc332098169fd83fb1
Mikhail <pervi@list.ru>
2022-12-15 23:35:37 +0700

    add force teleport

---
+++ Assets/_Sources/_Entities/ForceTeleport/Scripts/ForceTeleport.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceTeleport : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    
+    private bool _isPlayerFound;
+    private float _timeToFindPlayer;
+    private Fader _fader;
+
+    private void Awake()
+    {
+        _fader = FindObjectOfType<Fader>();
+    }
+
+    public void SetNewPlayerPosition(Vector3 position, bool isNeedFadeOut = true)
+    {
+        StartCoroutine(SetNewPlayerPositionCoroutine(position, isNeedFadeOut));
+    }
+    
+    public void SetNewPlayerPosition(Transform position, bool isNeedFadeOut = true)
+    {
+        StartCoroutine(SetNewPlayerPositionCoroutine(position.position, isNeedFadeOut));
+    }
+
+    private IEnumerator SetNewPlayerPositionCoroutine(Vector3 position, bool isNeedFadeOut = true)
+    {
+        float fadeTime = 0.3f;
+        _fader.FadeIn(fadeTime);
+        yield return new WaitForSeconds(fadeTime);
+        _player.position = position;
+
+        if (isNeedFadeOut)
+            _fader.FadeOut(fadeTime);
+    }
+}
